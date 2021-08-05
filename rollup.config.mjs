ab8cff3806ad206cf8e551dc85e2419b287333a6
Andreas Stassivik <andreas@stassi.net>
2021-08-04 17:42:00 -0700

    Revert "classic file extension export"
    This reverts commit c347cc17f5f57484ce9fd41840c048d5eef71654.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,7 +4,7 @@ import ts from 'rollup-plugin-ts'
 const rollupConfig = defineConfig({
   input: 'src/index.ts',
   output: {
-    file: 'lib/dechainer.js',
+    file: 'lib/dechainer.mjs',
     format: 'es',
     sourcemap: true,
   },
