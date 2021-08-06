30b011c0d753bad36a411e8aa292b2a7f9edf85e
Andreas Stassivik <andreas@stassi.net>
2021-08-06 13:28:19 -0700

    test name prefix prepending

--- src/functions/identity.test.ts
+++ src/functions/identity.test.ts
@@ -2,7 +2,7 @@ import identity from './identity'
 
 describe('identity', () => {
   describe.each([true, 1, Infinity, 'a', undefined])(
-    '%s',
+    'input: %s',
     (expected: unknown) => {
       it('should return a value identical to its parameter', () => {
         expect(identity(expected)).toStrictEqual(expected)
