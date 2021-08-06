a5fee1c0456d396cf36a7efa2c5ca97832509940
Andreas Stassivik <andreas@stassi.net>
2021-08-06 16:39:15 -0700

    successors implementation

---
+++ src/math/successors.ts
@@ -0,0 +1,8 @@
+import type { IdentityCallback } from '../functions'
+import { add } from './arithmetic'
+import negate from './negate'
+
+type NumberCallback = IdentityCallback<number>
+
+export const increment: NumberCallback = add(1)
+export const decrement: NumberCallback = add(negate(1))
