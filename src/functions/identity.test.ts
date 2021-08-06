e50493c230b56627d6cd967dd58bb500d27fb4d5
Andreas Stassivik <andreas@stassi.net>
2021-08-06 13:30:35 -0700

    identity test cases extension

--- src/functions/identity.test.ts
+++ src/functions/identity.test.ts
@@ -1,7 +1,7 @@
 import identity from './identity'
 
 describe('identity', () => {
-  describe.each([true, 1, Infinity, 'a', undefined])(
+  describe.each([true, 1, Infinity, 'a', undefined, null])(
     'input: %s',
     (expected: unknown) => {
       it('should return a value identical to its parameter', () => {
