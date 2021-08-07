3f01a1e9ff0bc4c77831afced7d0d6560592cd3e
Andreas Stassivik <andreas@stassi.net>
2021-08-06 19:13:48 -0700

    top-level test name

--- src/math/arithmetic.test.ts
+++ src/math/arithmetic.test.ts
@@ -1,85 +1,87 @@
 import { add, multiply } from './arithmetic'
 
-describe.each([
-  {
-    expected: {
-      product: 0,
-      sum: 0,
+describe('arithmetic', () => {
+  describe.each([
+    {
+      expected: {
+        product: 0,
+        sum: 0,
+      },
+      x: 0,
+      y: 0,
     },
-    x: 0,
-    y: 0,
-  },
-  {
-    expected: {
-      product: -0,
-      sum: -1,
+    {
+      expected: {
+        product: -0,
+        sum: -1,
+      },
+      x: -1,
+      y: 0,
     },
-    x: -1,
-    y: 0,
-  },
-  {
-    expected: {
-      product: 1,
-      sum: -2,
+    {
+      expected: {
+        product: 1,
+        sum: -2,
+      },
+      x: -1,
+      y: -1,
     },
-    x: -1,
-    y: -1,
-  },
-  {
-    expected: {
-      product: 0,
-      sum: 1,
+    {
+      expected: {
+        product: 0,
+        sum: 1,
+      },
+      x: 1,
+      y: 0,
     },
-    x: 1,
-    y: 0,
-  },
-  {
-    expected: {
-      product: 0,
-      sum: 1,
+    {
+      expected: {
+        product: 0,
+        sum: 1,
+      },
+      x: 0,
+      y: 1,
     },
-    x: 0,
-    y: 1,
-  },
-  {
-    expected: {
-      product: 0.25,
-      sum: 1,
+    {
+      expected: {
+        product: 0.25,
+        sum: 1,
+      },
+      x: 0.5,
+      y: 0.5,
     },
-    x: 0.5,
-    y: 0.5,
-  },
-  {
-    expected: {
-      product: 1,
-      sum: 2,
+    {
+      expected: {
+        product: 1,
+        sum: 2,
+      },
+      x: 1,
+      y: 1,
     },
-    x: 1,
-    y: 1,
-  },
-])(
-  'input: (x: $x, y: $y)',
-  ({
-    expected: { product: expectedProduct, sum: expectedSum },
-    x,
-    y,
-  }: {
-    expected: { product: number; sum: number }
-    x: number
-    y: number
-  }) => {
-    describe('add', () => {
-      it('should return the sum of two numbers', () => {
-        expect(add(x)(y)).toBe(expectedSum)
-        expect(add(x, y)).toBe(expectedSum)
+  ])(
+    'input: (x: $x, y: $y)',
+    ({
+      expected: { product: expectedProduct, sum: expectedSum },
+      x,
+      y,
+    }: {
+      expected: { product: number; sum: number }
+      x: number
+      y: number
+    }) => {
+      describe('add', () => {
+        it('should return the sum of two numbers', () => {
+          expect(add(x)(y)).toBe(expectedSum)
+          expect(add(x, y)).toBe(expectedSum)
+        })
       })
-    })
 
-    describe('multiply', () => {
-      it('should return the product of two numbers', () => {
-        expect(multiply(x)(y)).toBe(expectedProduct)
-        expect(multiply(x, y)).toBe(expectedProduct)
+      describe('multiply', () => {
+        it('should return the product of two numbers', () => {
+          expect(multiply(x)(y)).toBe(expectedProduct)
+          expect(multiply(x, y)).toBe(expectedProduct)
+        })
       })
-    })
-  }
-)
+    }
+  )
+})
