4b6eb77900088f55bfcfbde27b1ed049410b2563
Andreas Stassivik <andreas@stassi.net>
2021-08-06 19:14:25 -0700

    expected value nesting

--- src/math/rounding.test.ts
+++ src/math/rounding.test.ts
@@ -1,52 +1,63 @@
-import { ceiling as ceilingFn, floor as floorFn } from './rounding'
+import { ceiling, floor } from './rounding'
 
 describe('rounding', () => {
   describe.each([
     {
-      ceiling: -2,
-      floor: -2,
       input: -2,
+      expected: {
+        ceiling: -2,
+        floor: -2,
+      },
     },
     {
-      ceiling: -1,
-      floor: -2,
       input: -1.5,
+      expected: {
+        ceiling: -1,
+        floor: -2,
+      },
     },
     {
-      ceiling: 0,
-      floor: 0,
       input: 0,
+      expected: {
+        ceiling: 0,
+        floor: 0,
+      },
     },
     {
-      ceiling: 2,
-      floor: 1,
       input: 1.5,
+      expected: {
+        ceiling: 2,
+        floor: 1,
+      },
     },
     {
-      ceiling: 2,
-      floor: 2,
       input: 2,
+      expected: {
+        ceiling: 2,
+        floor: 2,
+      },
     },
   ])(
     'input: $input',
     ({
-      ceiling,
-      floor,
       input,
+      expected: { ceiling: expectedCeiling, floor: expectedFloor },
     }: {
-      ceiling: number
-      floor: number
       input: number
+      expected: {
+        ceiling: number
+        floor: number
+      }
     }) => {
       describe('ceiling', () => {
         it('should round a number up to the next largest integer', () => {
-          expect(ceilingFn(input)).toBe(ceiling)
+          expect(ceiling(input)).toBe(expectedCeiling)
         })
       })
 
       describe('floor', () => {
         it('should round a number down to the next smallest integer', () => {
-          expect(floorFn(input)).toBe(floor)
+          expect(floor(input)).toBe(expectedFloor)
         })
       })
     }
