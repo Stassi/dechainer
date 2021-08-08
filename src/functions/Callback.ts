3725babdaa7994129b67c8a0d629f1279a3a8a7b
Andreas Stassivik <andreas@stassi.net>
2021-08-08 01:29:58 -0700

    ReducerCallback implementation

--- src/functions/Callback.ts
+++ src/functions/Callback.ts
@@ -3,3 +3,5 @@ export type Callback<I, O> = (x: I) => O
 export type IdentityCallback<T> = Callback<T, T>
 
 export type NumberCallback = IdentityCallback<number>
+
+export type ReducerCallback<T> = Callback<T[], T>
