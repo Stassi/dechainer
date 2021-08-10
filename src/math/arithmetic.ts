f5e3bce14e1d18fbebb36228b2f89b7251df464a
Andreas Stassivik <andreas@stassi.net>
2021-08-10 09:39:29 -0700

    map integration

--- src/math/arithmetic.ts
+++ src/math/arithmetic.ts
@@ -1,7 +1,12 @@
+import type { Callback, NumberBinaryCallback } from '../functions'
 import hyperoperation from './hyperoperation'
+import { map } from '../iteration'
 import { negate, reciprocal } from './inversions'
 
 type Hyperoperation = ReturnType<typeof hyperoperation>
+type MapHyperoperation = Callback<NumberBinaryCallback[], Hyperoperation[]>
+
+const mapHyperoperation: MapHyperoperation = map(hyperoperation)
 
 export const [
   add,
@@ -10,11 +15,11 @@ export const [
   divide,
   exponentiate,
   remainder,
-]: Hyperoperation[] = [
+]: Hyperoperation[] = mapHyperoperation([
   (x: number, y: number): number => x + y,
   (x: number, y: number): number => negate(x) + y,
   (x: number, y: number): number => x * y,
   (x: number, y: number): number => reciprocal(x) * y,
   (x: number, y: number): number => x ** y,
   (x: number, y: number): number => y % x,
-].map(hyperoperation)
+])
