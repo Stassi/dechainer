0b104028fb9dd4e5221776420ffda42153a22469
Andreas Stassivik <andreas@stassi.net>
2021-08-10 09:39:13 -0700

    map implementation

--- src/iteration.ts
+++ src/iteration.ts
@@ -1,5 +1,15 @@
+import type { Callback } from './functions'
+
+interface Mapper<I, O> {
+  (value: I, index: number, array: I[]): O
+}
+
 interface Reducer<T> {
   (previousValue: T, currentValue: T, currentIndex: number, array: T[]): T
+}
+
+export function map<I, O>(mapper: Mapper<I, O>): Callback<I[], O[]> {
+  return (a: I[]): O[] => a.map(mapper)
 }
 
 export function reduce<T>(reducer: Reducer<T>) {
