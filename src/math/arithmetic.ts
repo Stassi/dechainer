8b9f867645ee03293a79eeaf83d9bd304d2832df
Andreas Stassivik <andreas@stassi.net>
2021-08-13 12:08:59 -0700

    arithmetic hyperoperations inlining

--- src/math/arithmetic.ts
+++ src/math/arithmetic.ts
@@ -1,26 +1,47 @@
 import type {
   MapCallback,
   NumberBinaryCallback,
-  NumberCurriedCallback,
+  NumberBinaryCurried,
+  NumberNaryCurried,
 } from '../functions'
-import hyperoperation from './hyperoperation'
-import { map } from '../iteration'
+import { head } from '../arrayAccessors'
+import length from '../length'
 import { negate, reciprocal } from './inversions'
+import { map, reduce } from '../iteration'
 
-const mapHyperoperation: MapCallback<
-  NumberBinaryCallback,
-  NumberCurriedCallback
-> = map(hyperoperation)
+const mapNAryOperation: MapCallback<NumberBinaryCallback, NumberNaryCurried> =
+  map(
+    (operation: NumberBinaryCallback): NumberNaryCurried =>
+      <NumberNaryCurried>(
+        ((...n: number[]) =>
+          length(n) === 1
+            ? (x: number): number => operation(head(n), x)
+            : reduce(operation)(n))
+      )
+  )
 
-export const [add, multiply, exponentiate]: NumberCurriedCallback[] =
-  mapHyperoperation([
+export const [add, multiply, exponentiate]: NumberNaryCurried[] =
+  mapNAryOperation([
     (x: number, y: number): number => x + y,
     (x: number, y: number): number => x * y,
     (x: number, y: number): number => x ** y,
   ])
 
-export const [subtract, divide, remainder]: NumberCurriedCallback[] =
-  mapHyperoperation([
+const mapBinaryOperation: MapCallback<
+  NumberBinaryCallback,
+  NumberBinaryCurried
+> = map(
+  (operation: NumberBinaryCallback): NumberBinaryCurried =>
+    <NumberBinaryCurried>(
+      ((...n: number[]) =>
+        length(n) === 1
+          ? (x: number): number => operation(head(n), x)
+          : reduce(operation)(n))
+    )
+)
+
+export const [subtract, divide, remainder]: NumberBinaryCurried[] =
+  mapBinaryOperation([
     (x: number, y: number): number => y + negate(x),
     (x: number, y: number): number => y * reciprocal(x),
     (x: number, y: number): number => y % x,
