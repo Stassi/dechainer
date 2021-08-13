e6256bfc2962ff01317ccdc0c5db6207f98fd38e
Andreas Stassivik <andreas@stassi.net>
2021-08-13 12:17:15 -0700

    exponentiate named export

--- src/math/index.ts
+++ src/math/index.ts
@@ -1,4 +1,11 @@
-import { add, divide, multiply, remainder, subtract } from './arithmetic'
+import {
+  add,
+  divide,
+  exponentiate,
+  multiply,
+  remainder,
+  subtract,
+} from './arithmetic'
 import { negate, reciprocal } from './inversions'
 import { ceiling, floor } from './rounding'
 import { decrement, increment } from './successors'
@@ -8,6 +15,7 @@ export {
   ceiling,
   decrement,
   divide,
+  exponentiate,
   floor,
   increment,
   multiply,
