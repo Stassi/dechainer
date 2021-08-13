e752c2fdaa2f6b64b1c6cbdc87766e7070b4c747
Andreas Stassivik <andreas@stassi.net>
2021-08-13 12:07:00 -0700

    type definitions: IdentityBinaryCurried, IdentityNaryCurried, NumberBinaryCurried, NumberNaryCurried

--- src/functions/Callback.ts
+++ src/functions/Callback.ts
@@ -2,14 +2,23 @@ export type Callback<I, O> = (x: I) => O
 
 export type IdentityCallback<T> = Callback<T, T>
 export type IdentityBinaryCallback<T> = (x: T, y: T) => T
-export type IdentityCurriedCallback<T> = {
+
+export type IdentityCurried<T> = {
   (x: T): IdentityCallback<T>
+}
+
+export type IdentityBinaryCurried<T> = IdentityCurried<T> & {
+  (x: T, y: T): T
+}
+
+export type IdentityNaryCurried<T> = IdentityCurried<T> & {
   (...x: T[]): T
 }
 
 export type NumberCallback = IdentityCallback<number>
 export type NumberBinaryCallback = IdentityBinaryCallback<number>
-export type NumberCurriedCallback = IdentityCurriedCallback<number>
+export type NumberBinaryCurried = IdentityBinaryCurried<number>
+export type NumberNaryCurried = IdentityNaryCurried<number>
 
 export type MapCallback<I, O> = Callback<I[], O[]>
 export type ReducerCallback<T> = Callback<T[], T>
