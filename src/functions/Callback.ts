3c5ca8be2337225e009bf1c12a695303c1c01bf7
Andreas Stassivik <andreas@stassi.net>
2021-08-14 00:04:26 -0700

    type definitions: CallbackBinary, CallbackCurried

--- src/functions/Callback.ts
+++ src/functions/Callback.ts
@@ -1,5 +1,7 @@
 export type Callback<T, U> = (x: T) => U
 export type CallbackOptional<T, U> = (x?: T) => U
+export type CallbackBinary<T, U, V> = (x: T, y: U) => V
+export type CallbackCurried<T, U, V> = (a: T) => (b: U) => V
 
 export type MapCallback<T, U> = Callback<T[], U[]>
 export type ReduceCallback<T> = Callback<T[], T>
