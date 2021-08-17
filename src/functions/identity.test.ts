f95eb96f34711fbf25021ced747b65f6e9ffb03d
Andreas Stassivik <andreas@stassi.net>
2021-08-17 01:18:31 -0700

    identity ±0 tests

--- src/functions/identity.test.ts
+++ src/functions/identity.test.ts
@@ -1,7 +1,7 @@
 import identity from './identity'
 
 describe('identity', () => {
-  describe.each([0n, true, null, 1, Infinity, NaN, 'a', undefined])(
+  describe.each([0n, true, null, -0, 0, Infinity, NaN, 'a', undefined])(
     'input: %s',
     (expected: bigint | boolean | null | number | string | undefined) => {
       it('should return a value identical to its parameter', () => {
