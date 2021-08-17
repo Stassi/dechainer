f6ab4cd2aea5b6cc1e40dda9809f15a0f1885f3c
Andreas Stassivik <andreas@stassi.net>
2021-08-17 01:14:23 -0700

    explicit expected type

--- src/functions/identity.test.ts
+++ src/functions/identity.test.ts
@@ -3,7 +3,7 @@ import identity from './identity'
 describe('identity', () => {
   describe.each([0n, true, null, 1, Infinity, NaN, 'a', undefined])(
     'input: %s',
-    (expected: unknown) => {
+    (expected: bigint | boolean | null | number | string | undefined) => {
       it('should return a value identical to its parameter', () => {
         expect(identity(expected)).toStrictEqual(expected)
       })
