4d0c4c8adfb8ebaa92ed1df14be1e6781ffedc21
Andreas Stassivik <andreas@stassi.net>
2021-08-16 21:00:42 -0700

    iteration iterable test cases extension

--- src/iteration.test.ts
+++ src/iteration.test.ts
@@ -3,67 +3,81 @@ import { map, reduce } from './iteration'
 import { add, decrement, increment, multiply } from './math'
 
 describe('iteration', () => {
-  const input: number[] = [1, 2, 3, 4]
+  describe.each([
+    {
+      input: [1, 2, 3],
+      expected: {
+        add: 6,
+        decrement: [0, 1, 2],
+        increment: [2, 3, 4],
+        multiply: 6,
+      },
+    },
+    {
+      input: [2, 4, 6],
+      expected: {
+        add: 12,
+        decrement: [1, 3, 5],
+        increment: [3, 5, 7],
+        multiply: 48,
+      },
+    },
+  ])(
+    'input: $input',
+    ({
+      input,
+      expected,
+    }: {
+      input: number[]
+      expected: Record<string, number | number[]>
+    }) => {
+      describe('map(...)', () => {
+        describe.each([
+          {
+            mapper: decrement,
+            name: 'decrement',
+          },
+          {
+            mapper: increment,
+            name: 'increment',
+          },
+        ])(
+          'map($name)',
+          ({ mapper, name }: { mapper: NumberCallback; name: string }) => {
+            it(`should return ${name}ed values`, () => {
+              expect(map(mapper)(input)).toStrictEqual(expected[name])
+              expect(map(mapper, input)).toStrictEqual(expected[name])
+            })
+          }
+        )
+      })
 
-  describe('map(...)', () => {
-    describe.each([
-      {
-        expected: [0, 1, 2, 3],
-        mapper: decrement,
-        name: 'decrement',
-      },
-      {
-        expected: [2, 3, 4, 5],
-        mapper: increment,
-        name: 'increment',
-      },
-    ])(
-      'map($name)',
-      ({
-        expected,
-        mapper,
-        name,
-      }: {
-        expected: number[]
-        mapper: NumberCallback
-        name: string
-      }) => {
-        it(`should return ${name}ed values`, () => {
-          expect(map(mapper)(input)).toStrictEqual(expected)
-          expect(map(mapper, input)).toStrictEqual(expected)
-        })
-      }
-    )
-  })
-
-  describe('reduce(...)', () => {
-    describe.each([
-      {
-        expected: 10,
-        name: 'add',
-        reducer: (x: number, y: number): number => add(x, y),
-      },
-      {
-        expected: 24,
-        name: 'multiply',
-        reducer: (x: number, y: number): number => multiply(x, y),
-      },
-    ])(
-      'reduce($name)',
-      ({
-        expected,
-        name,
-        reducer,
-      }: {
-        expected: number
-        name: string
-        reducer: NumberBinaryCallback
-      }) => {
-        it(`should return ${name}ed values`, () => {
-          expect(reduce(reducer)(input)).toBe(expected)
-          expect(reduce(reducer, input)).toBe(expected)
-        })
-      }
-    )
-  })
+      describe('reduce(...)', () => {
+        describe.each([
+          {
+            name: 'add',
+            reducer: (x: number, y: number): number => add(x, y),
+          },
+          {
+            name: 'multiply',
+            reducer: (x: number, y: number): number => multiply(x, y),
+          },
+        ])(
+          'reduce($name)',
+          ({
+            name,
+            reducer,
+          }: {
+            name: string
+            reducer: NumberBinaryCallback
+          }) => {
+            it(`should return ${name}ed values`, () => {
+              expect(reduce(reducer)(input)).toBe(expected[name])
+              expect(reduce(reducer, input)).toBe(expected[name])
+            })
+          }
+        )
+      })
+    }
+  )
 })
