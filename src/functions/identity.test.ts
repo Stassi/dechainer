cf25d64640aca15bebd0f6525769cbe83fcb193d
Andreas Stassivik <andreas@stassi.net>
2021-08-17 01:38:43 -0700

    identity: (symbol | void) tests

--- src/functions/identity.test.ts
+++ src/functions/identity.test.ts
@@ -1,10 +1,32 @@
 import identity from './identity'
 
 describe('identity', () => {
-  describe.each([[], 0n, true, null, -0, 0, Infinity, NaN, 'a', undefined])(
+  describe.each([
+    [],
+    0n,
+    true,
+    null,
+    -0,
+    0,
+    Infinity,
+    NaN,
+    'a',
+    Symbol('a'),
+    undefined,
+    () => {},
+  ])(
     'input: %s',
     (
-      expected: any[] | bigint | boolean | null | number | string | undefined
+      expected:
+        | any[]
+        | bigint
+        | boolean
+        | null
+        | number
+        | string
+        | symbol
+        | undefined
+        | (() => void)
     ) => {
       it('should return a value identical to its parameter', () => {
         expect(identity(expected)).toStrictEqual(expected)
