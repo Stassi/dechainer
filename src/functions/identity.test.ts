54d3ffa6f24f3cf11d2e1829f76d934b5c3283fe
Andreas Stassivik <andreas@stassi.net>
2021-08-17 01:20:50 -0700

    identity array test

--- src/functions/identity.test.ts
+++ src/functions/identity.test.ts
@@ -1,9 +1,11 @@
 import identity from './identity'
 
 describe('identity', () => {
-  describe.each([0n, true, null, -0, 0, Infinity, NaN, 'a', undefined])(
+  describe.each([[], 0n, true, null, -0, 0, Infinity, NaN, 'a', undefined])(
     'input: %s',
-    (expected: bigint | boolean | null | number | string | undefined) => {
+    (
+      expected: any[] | bigint | boolean | null | number | string | undefined
+    ) => {
       it('should return a value identical to its parameter', () => {
         expect(identity(expected)).toStrictEqual(expected)
       })
