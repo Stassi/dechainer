8aa185702f46bf759c97cda992a1fb3a29e4254d
Andreas Stassivik <andreas@stassi.net>
2021-08-21 10:32:58 -0700

    array predicates implementation

---
+++ src/arrayPredicates.ts
@@ -0,0 +1,19 @@
+import type { Callback, Predicate } from './functions'
+
+export function every<T>(predicate: Predicate<T>): Predicate<T[]>
+export function every<T>(predicate: Predicate<T>, x: T[]): boolean
+export function every<T>(
+  predicate: Predicate<T>,
+  x?: T[]
+): boolean | Callback<T[], boolean> {
+  return x ? x.every(predicate) : (y: T[]): boolean => y.every(predicate)
+}
+
+export function some<T>(predicate: Predicate<T>): Predicate<T[]>
+export function some<T>(predicate: Predicate<T>, x: T[]): boolean
+export function some<T>(
+  predicate: Predicate<T>,
+  x?: T[]
+): boolean | Callback<T[], boolean> {
+  return x ? x.some(predicate) : (y: T[]): boolean => y.some(predicate)
+}
