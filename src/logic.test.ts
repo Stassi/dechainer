9a210477fcfff236821f836bcca93288432b6a37
Andreas Stassivik <andreas@stassi.net>
2021-08-21 18:38:05 -0700

    strict equality merged into logic test

--- src/logic.test.ts
+++ src/logic.test.ts
@@ -1,4 +1,5 @@
-import { not } from './logic'
+import { add } from './math'
+import { not, strictEquality } from './logic'
 
 describe('logic', () => {
   describe('not', () => {
@@ -16,6 +17,30 @@ describe('logic', () => {
       ({ expected, input }: Record<'expected' | 'input', boolean>) => {
         it(`should be inverted as ${expected}`, () => {
           expect(not(input)).toBe(expected)
+        })
+      }
+    )
+  })
+
+  describe('strict equality', () => {
+    describe.each([
+      {
+        expected: 'a',
+        input: 'a',
+      },
+      {
+        expected: 1,
+        input: 1,
+      },
+      {
+        expected: 2,
+        input: add(1, 1),
+      },
+    ])(
+      'input: $input',
+      ({ expected, input }: Record<'expected' | 'input', number | string>) => {
+        it('should be strictly equal to its parameter', () => {
+          expect(strictEquality(input)(expected)).toBeTruthy()
         })
       }
     )
