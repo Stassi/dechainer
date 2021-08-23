f31cb07d7d6b82bb2cf110af16e98ec4bf839b9b
Andreas Stassivik <andreas@stassi.net>
2021-08-23 16:34:20 -0700

    array accessors semi-currying

--- src/arrayAccessors.ts
+++ src/arrayAccessors.ts
@@ -1,7 +1,9 @@
 import type { ReduceCallback } from './functions'
 
-export function atIndex<T>(i: number): ReduceCallback<T> {
-  return (a: T[]): T => a[i]
+export function atIndex<T>(x: number): ReduceCallback<T>
+export function atIndex<T>(x: number, y: T[]): T
+export function atIndex<T>(x: number, y?: T[]): T | ReduceCallback<T> {
+  return y ? y[x] : (z: T[]): T => z[x]
 }
 
 export const head: <T>(a: T[]) => T = atIndex(0)
