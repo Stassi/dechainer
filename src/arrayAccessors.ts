7a87bd8ceba1c1276994edc7bad31692830ea4d3
Andreas Stassivik <andreas@stassi.net>
2021-08-23 18:46:16 -0700

    indexOf implementation

--- src/arrayAccessors.ts
+++ src/arrayAccessors.ts
@@ -1,9 +1,15 @@
-import type { ReduceCallback } from './functions'
+import type { Callback, ReduceCallback } from './functions'
 
 export function atIndex<T>(x: number): ReduceCallback<T>
 export function atIndex<T>(x: number, y: T[]): T
 export function atIndex<T>(x: number, y?: T[]): T | ReduceCallback<T> {
   return y ? y[x] : (z: T[]): T => z[x]
+}
+
+export function indexOf<T>(x: T[]): Callback<number, T>
+export function indexOf<T>(x: T[], y: number): T
+export function indexOf<T>(x: T[], y?: number): T | Callback<number, T> {
+  return typeof y === 'number' ? x[y] : (z: number): T => x[z]
 }
 
 export const head: <T>(a: T[]) => T = atIndex(0)
