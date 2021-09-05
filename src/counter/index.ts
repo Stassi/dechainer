642173872607a728e4bf31bf669282d2ef9c62f9
Andreas Stassivik <andreas@stassi.net>
2021-09-05 12:42:12 -0700

    counter (im-)persistence named exports

---
+++ src/counter/index.ts
@@ -0,0 +1,7 @@
+import type { CounterImpersistent } from './counterImpersistent'
+import type { CounterPersistent } from './counterPersistent'
+import counterImpersistent from './counterImpersistent'
+import counterPersistent from './counterPersistent'
+
+export type { CounterImpersistent, CounterPersistent }
+export { counterImpersistent, counterPersistent }
